8d6264ddb13370250b58d2e5b7b9cb12df71e2b9
Peter Hilton <pedro+git@lunatech.com>
2012-12-08 18:35:51 +0100

    Fix typo

--- ch04-defining-the-application-http-interface/controllers/app/controllers/Application.scala
+++ ch04-defining-the-application-http-interface/controllers/app/controllers/Application.scala
@@ -8,7 +8,7 @@ object Application extends Controller {
   def home = Action {
     Ok("<h1>Controller actions</h1>" +
       "<p>This application only demonstrates action method declarations " +
-      "and does generate any output, other than this message.</p>" +
+      "and does not generate any output, other than this message.</p>" +
       "<p>See <tt>app/controllers/Products.scala</tt> for the sample code.</p>").as("text/html")
   }
 }
