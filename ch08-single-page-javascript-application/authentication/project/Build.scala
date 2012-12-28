8f9d64482bfb9a96ba50b503004cc562cf5da7e0
Peter Hilton <pedro+git@lunatech.com>
2012-12-28 17:29:09 +0100

    Update to Play 2.1-RC1

--- ch08-single-page-javascript-application/authentication/project/Build.scala
+++ ch08-single-page-javascript-application/authentication/project/Build.scala
@@ -1,18 +1,18 @@
 import sbt._
 import Keys._
-import PlayProject._
+import play.Project._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "authentication"
-    val appVersion      = "1.0-SNAPSHOT"
+  val appName         = "authentication"
+  val appVersion      = "1.0-SNAPSHOT"
 
-    val appDependencies = Seq(
-      // Add your project dependencies here,
-    )
+  val appDependencies = Seq(
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
-    )
+  )
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+  
+  )
 
 }
