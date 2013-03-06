5ac101f440f5c48d4fa4daae1bc4b7396a91f20d
Francisco José Canedo Dominguez <paco+git@lunatech.com>
2013-03-06 18:07:54 +0100

    Adds module sample

---
+++ ch09-play-and-more/ean-module/app/com/github/playforscala/barcodes/Barcodes.scala
@@ -0,0 +1,43 @@
+package com.github.playforscala.barcodes
+
+import play.api.mvc.{Action, Controller}
+import org.krysalis.barcode4j.output.bitmap.BitmapCanvasProvider
+import org.krysalis.barcode4j.impl.upcean.EAN13Bean
+
+
+object Barcodes extends Controller {
+
+  val ImageResolution = 144
+
+  def barcode(ean: Long) = Action {
+
+    import java.lang.IllegalArgumentException
+
+    val MimeType = "image/png"
+    try {
+      val imageData = ean13BarCode(ean, MimeType)
+      Ok(imageData).as(MimeType)
+    }
+    catch {
+      case e: IllegalArgumentException =>
+        BadRequest("Couldn’t generate bar code. Error: " + e.getMessage)
+    }
+  }
+
+  def ean13BarCode(ean: Long, mimeType: String): Array[Byte] = {
+
+    import java.io.ByteArrayOutputStream
+    import java.awt.image.BufferedImage
+
+    var output: ByteArrayOutputStream = new ByteArrayOutputStream
+    var canvas: BitmapCanvasProvider =
+      new BitmapCanvasProvider(output, mimeType, ImageResolution,
+        BufferedImage.TYPE_BYTE_BINARY, false, 0)
+
+    val barCode = new EAN13Bean()
+    barCode.generateBarcode(canvas, String valueOf ean)
+    canvas.finish
+
+    output.toByteArray
+  }
+}
