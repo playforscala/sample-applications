fb9ed14f315b74f5b015d7b7b7e285584bf2809a
Le Zhang <zhale@amazon.com>
2020-01-03 08:09:45 -0800

    adding back the template sample

--- ch01-introduction-to-play-2/hello/app/controllers.scala
+++ ch01-introduction-to-play-2/hello/app/controllers.scala
@@ -16,3 +16,14 @@ class Application @Inject()(cc: ControllerComponents) extends AbstractController
     Ok("Hello " + name)
   }
 }
+
+/**
+ * Alternate controller that renders a template. This example is a separate class so the action method name
+ * can also be ‘hello’.
+ */
+class Application2 @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+  def hello(name: String) = Action {
+    Ok(views.html.hello(name))
+  }
+}
