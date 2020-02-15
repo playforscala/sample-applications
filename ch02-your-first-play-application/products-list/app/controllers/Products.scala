ed4a9b705c389bb9204b5f01d295539534c439d1
Le Zhang <zhangletju@gmail.com>
2020-02-15 00:08:44 -0800

    adding back config and environment variable

--- ch02-your-first-play-application/products-list/app/controllers/Products.scala
+++ ch02-your-first-play-application/products-list/app/controllers/Products.scala
@@ -4,7 +4,7 @@ import javax.inject._
 import play.api.mvc._
 import models.Product
 
-class Products @Inject()(cc: ControllerComponents)
+class Products @Inject()(cc: ControllerComponents)(c: play.api.Configuration)
   extends AbstractController(cc)
   with play.api.i18n.I18nSupport {
   
@@ -12,6 +12,7 @@ class Products @Inject()(cc: ControllerComponents)
 
     val products = Product.findAll
 
+    implicit lazy val config = c
     Ok(views.html.products.list(products))
   }
 }
