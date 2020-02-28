4ed69a71c15ffdf745b5552b3571291e2a665b9b
Le Zhang <zhangletju@gmail.com>
2020-02-28 00:25:08 -0800

    finished all ch02 upgrade

--- ch02-your-first-play-application/first-sample/app/controllers/Application.scala
+++ ch02-your-first-play-application/first-sample/app/controllers/Application.scala
@@ -9,12 +9,13 @@ import play.api.Configuration
 /**
  * Main application controller.
  */
-class Application @Inject()(val configuration: Configuration) extends Controller {
+class Application @Inject()(cc: ControllerComponents)
+  extends AbstractController(cc) {
 
   /**
    * Redirect to the product list.
    */
-  def index = Action { implicit request =>
+  def index = Action { // implicit request =>
     Redirect(routes.Products.list())
   }
 }
